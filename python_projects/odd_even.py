92c8b28a8e40f933aa1501499576a51bbb725cee
Immanuel Njogu <njoguimmanuel8@gmail.com>
2024-11-12 21:11:45 +0300

    task 2 odd or even

---
+++ python_projects/odd_even.py
@@ -0,0 +1,12 @@
+#task 2 
+# we are creating a file that asks the user for a number and tells them if it is odd or even
+
+number = int(input("Enter a number:"))
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+
+
