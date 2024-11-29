eb43d544b2418424cb8e865409ed489086c5f48f
Immanuel Njogu <njoguimmanuel8@gmail.com>
2024-11-29 20:40:47 +0300

    changes

--- Django_projects/myproject/myproject/settings.py
+++ Django_projects/myproject/myproject/settings.py
@@ -77,14 +77,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'mydatabase',
-        'USER': 'root',
-        'PASSWORD':'Immamanu12!',
-        'HOST': 'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
